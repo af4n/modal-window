0c93b30f230ec57d30dd8804b9331b76ef6ecc4f
Aleksey Afanaskin <aleksey.afanaskin@gmail.com>
2020-05-25 00:31:47 +0300

    method destroy and cards fruits was added

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const modal = $.modal()
+const fruits = [
+  {id: 1, title: 'Яблоки', price: 20, img: 'https://lifeglobe.net/x/entry/6259/1a-0.jpg'},
+  {id: 1, title: 'Апельсины', price: 30, img: 'https://bipbap.ru/wp-content/uploads/2018/07/apelsini.jpg'},
+  {id: 1, title: 'Манго', price: 40, img: 'https://st.depositphotos.com/3260227/4344/i/950/depositphotos_43441807-stock-photo-mango-fruit.jpg'}
+]
+
+const modal = $.modal({
+  title: 'Modal',
+  closable: true,
+  content: `
+    <p>Modal is working</p>
+    <p>Modal text</p>
+  `,
+  width: '400px',
+  footerButtons: [
+    {text: 'Ok', type: 'primary', handler() {
+      console.log('Primary btn clicked')
+      modal.close()
+    }},
+    {text: 'Cencel', type: 'danger', handler() {
+      console.log('Danger btn clicked')
+      modal.close()
+    }}
+  ]
+})
